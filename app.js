bfc933df2e7c9a8a4b1b1fbddfcffba365f1678e
Adhisetama <adhisetama@gmail.com>
2022-09-15 23:18:47 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+// EJS view engine
+app.use(express.urlencoded())
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// koneksi dengan database
+const { Medicine, MedicineLog } = require('./utils/db')
+
+
+
+// halaman index
+app.get('/', async (req, res) => {
+    medicines = await Medicine.find() 
+    res.render('index', {
+        medicines
+    })
+})
+
+app.get('/tambah-obat', async (req, res) => {
+    res.render('tambah-obat')
+})
+
+
+app.listen(PORT, () => {
+    console.log('listening to port ' + PORT)
+})
---
+++ utils/db.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const uri = "mongodb+srv://admin:admin@testmongo.rbvgcyd.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// model struktur database
+const Medicine = mongoose.model('medicine', {
+    name: { type: String },
+    manufacturer: { type: String },
+    description: { type: String },
+    stock: { type: Number },
+    log: { type: Array }
+})
+
+class MedicineLog {
+    MedicineLog(stock_io, date=new Date().toLocaleString(), description="") {
+        this.stock_io = stock_io
+        this.date = date
+        this.description = description
+    }
+}
+
+module.exports = { Medicine, MedicineLog }
